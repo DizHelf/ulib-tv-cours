5b1bc47844700ac510040a3c76c8824808e941a1
DizHelf <mustangzxc@inbox.ru>
2023-09-01 16:07:09 +0300

    lesson 17 add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -26,6 +26,26 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            "i18next-extract", 
+            {
+              locales: ["ru", "en"],
+              "keyAsDefaultValue": true
+            }
+          ],
+        ]
+      }
+    }
+  }
+
   const sassLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -46,9 +66,10 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   }
 
   return [
-    typescriptLoader,
-    sassLoader,
     fileLoader,
-    svgLoader
+    svgLoader,
+    babelLoader,
+    typescriptLoader,
+    sassLoader
   ]
 }
